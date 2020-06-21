55b1d4674bce80387d68270dd16cb28ee0a2fb8f
”KA-gamecreate” <burakki.efi.22@docomo.ne.jp>
2020-06-21 17:25:49 +0900

    “キャラ変更まで終わった”

---
+++ Assets/Charachange.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charachange : MonoBehaviour
+{
+    public bool charaA = false;
+    public bool charaB = false;
+    public static Charachange charachange;
+
+    public GameObject ghost;
+    public GameObject human;
+   
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        charaA = true;
+        charachange = this;
+        human.gameObject.SetActive(true);
+        ghost.gameObject.SetActive(false);
+        gameObject.AddComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            charaA = !charaA;
+            charaB = !charaB;
+        }
+
+       if(charaA == true)
+        {
+            human.gameObject.SetActive(true);
+            ghost.gameObject.SetActive(false);
+            gameObject.AddComponent<Rigidbody2D>();
+        }
+
+       else if(charaB == true)
+        {
+            ghost.gameObject.SetActive(true);
+            human.gameObject.SetActive(false);
+            Destroy(GetComponent<Rigidbody2D>());
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    
+
+    float speed = 100.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+
+            if (Input.GetKey("right"))
+            {
+                transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            }
+
+
+
+            if (Input.GetKey("left"))
+            {
+                transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+            }
+        
+
+    }
+}
+
