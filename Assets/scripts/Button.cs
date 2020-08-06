d4a5fafb5086fe1cffbc7a28cb96d72d7ba7ff68
”KA-gamecreate” <burakki.efi.22@docomo.ne.jp>
2020-08-06 14:12:48 +0900

    簡易的な選択画面まで終わった

--- Assets/scripts/Button.cs
+++ Assets/scripts/Button.cs
@@ -33,5 +33,46 @@ public class Button : MonoBehaviour
     {
         SceneManager.LoadScene("Tutorial2");
     }
+    public void Stage3()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+    public void Stage4()
+    {
+        SceneManager.LoadScene("Stage2");
+    }
+    public void Stage5()
+    {
+        SceneManager.LoadScene("Stage3");
+    }
+    public void Stage6()
+    {
+        SceneManager.LoadScene("Stage4");
+    }
+    public void Stage7()
+    {
+        SceneManager.LoadScene("Stage5");
+    }
+    public void Stage8()
+    {
+        SceneManager.LoadScene("Stage6");
+    }
+    public void Stage9()
+    {
+        SceneManager.LoadScene("Stage7");
+    }
+    public void Stage10()
+    {
+        SceneManager.LoadScene("Stage8");
+    }
+    public void Stage11()
+    {
+        SceneManager.LoadScene("Stage9");
+    }
+    public void Stage12()
+    {
+        SceneManager.LoadScene("Stage10");
+    }
+
 }
 
